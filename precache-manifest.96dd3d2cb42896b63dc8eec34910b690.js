cdb076d3cf222216543cfd9a91ace5399d9474b7
Punnadit Trussat <punnadittr@gmail.com>
2018-11-30 17:37:36 +0700

    Updates

---
+++ precache-manifest.96dd3d2cb42896b63dc8eec34910b690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9ecd07fee8813dcd6acf060b5db8414",
+    "url": "/abhome/static/media/footergraphic.a9ecd07f.svg"
+  },
+  {
+    "revision": "b16dd8ce172367c627f6",
+    "url": "/abhome/static/js/runtime~main.b16dd8ce.js"
+  },
+  {
+    "revision": "4961af55f684e44022ea",
+    "url": "/abhome/static/js/main.4961af55.chunk.js"
+  },
+  {
+    "revision": "7ec77e0230c0d586c7da",
+    "url": "/abhome/static/js/1.7ec77e02.chunk.js"
+  },
+  {
+    "revision": "4961af55f684e44022ea",
+    "url": "/abhome/static/css/main.102fb2d2.chunk.css"
+  },
+  {
+    "revision": "7f7c8c0e2023cd8748ad2f3bfe03ccf4",
+    "url": "/abhome/index.html"
+  }
+];
